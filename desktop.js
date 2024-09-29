1ad73f483748f338c5f658c7b6a3705c1417ca31
Yoanna Klinchova <108792686+klinchoni@users.noreply.github.com>
2024-09-29 14:49:50 +0300

    Adaptive Site

---
+++ desktop.js
@@ -0,0 +1,80 @@
+const desktopStyle = `
+body {
+	font-size: 18px;
+}
+
+.home-container 
+.navigation-header 
+.navigation-max-width
+.navigation-nav 
+.navigation-links-nav .navigation-links-root-class-name17
+.button-secondary .button .button-md
+.home-main
+.home-portofolio .section-container
+.home-max-width .max-content-container
+.home-text-container
+.home-text
+.home-text01 .Heading2
+.home-text02
+.button .button-lg .button-secondary
+.home-tab-selector-header
+.home-text03 .tab-selector-btn
+.home-tab-selector-cards-container
+.portofolio-card-speaker-card .portofolio-card-root-class-name
+.portofolio-card-see-project-container
+.button-secondary-white
+.portofolio-card-first-name
+.portofolio-card-root-class-name5
+.portofolio-card-root-class-name4
+.portofolio-card-root-class-name3
+.portofolio-card-root-class-name2
+.portofolio-card-root-class-name1
+.home-about .section-container
+.home-max-width1 .max-content-container
+.home-text-container
+.home-text04
+.home-text05
+.home-text06
+.home-checklist
+.home-check-item
+.fa-solid .fa-check
+.home-text07
+.home-check-item
+.home-image-container
+.home-image
+.home-process .section-container
+.home-max-width2
+.home-text08
+.home-text09
+.home-step
+.home-text10
+.home-text11
+.home-text12
+.home-container1
+.home-top-part
+.home-links-container
+.home-product-container
+.home-text13
+.home-text14
+.home-navigate-container
+.home-contact-container
+.home-subscribe-container
+.button-primary
+.button-container{
+	width: 80%;
+	margin: 0 auto;
+	padding: 30px;
+}
+
+.portofolio-card-image-container
+.portofolio-card-image
+img {
+	max-width: 100%;
+	height: auto;
+}
+`;
+
+const styleSheet = document.createElement("style");
+styleSheet.type = "text/css";
+styleSheet.innerText = desktopStyles;
+document.head.appendChild(styleSheet);
